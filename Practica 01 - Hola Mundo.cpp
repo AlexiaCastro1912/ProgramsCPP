ee338d8ee5b554495682f6b51206b211fd7f4bb2
Alexia Castro <alexia.castro1912@outlook.com>
2025-02-12 23:24:21 -0600

    HollaMundi
    Programa escueto

---
+++ Practica 01 - Hola Mundo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+//using namespace std;
+
+//Esto es un comentario individual
+
+/*
+Esto es
+un comentario
+de varias lineas
+*/
+
+
+int main() 
+{
+	std::cout<<"Hola mundo, bienvenido a Programacion I";
+	
+	return 0;
+}
